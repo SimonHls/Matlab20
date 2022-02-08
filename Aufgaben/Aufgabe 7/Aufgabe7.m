9706d13cc6244e0b55f9ac30e79852322752075e
SimonHls <simon.huelskamp@gmail.com>
2022-02-08 12:57:48 +0100

    Aufgabe 7 Performance

--- Aufgaben/Aufgabe 7/Aufgabe7.m
+++ Aufgaben/Aufgabe 7/Aufgabe7.m
@@ -5,8 +5,19 @@ function Aufgabe7()
 
 clear all, clc
 
+% Symbol x deklarieren. (Funktioniert nur mit Symbolic Math Toolbox) 
 syms x;
+
+% Performance erhöhen durch weniger Stellen (1.6s vs 2.3s)
+digits(32);
+
+% Gleichung 
 eqn = x.*log(0.1.*x) == tan(x);
+
+%Lösung in solution speichern und ausgeben.
 solution = getAllSolutions(eqn, x, [0,10], 10);
 disp('Gefundene Lösungen: ');
 disp(solution);
+
+% Stellen zurücksetzen
+digits(32);
