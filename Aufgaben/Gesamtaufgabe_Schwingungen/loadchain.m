618938c49ec75306ce96152a2e9f3bd5c98d583a
SimonHls <simon.huelskamp@gmail.com>
2022-02-08 15:05:55 +0100

    loadchain mit ehlerbeschreibung ergänzt

--- Aufgaben/Gesamtaufgabe_Schwingungen/loadchain.m
+++ Aufgaben/Gesamtaufgabe_Schwingungen/loadchain.m
@@ -1,4 +1,4 @@
-function [N, m, c, y] = loadchain(filename)
+function [m, c, y] = loadchain(filename)
 % Lädt Parameter aus einer .chain Datei
 % Parameter:
 %   filename      Name der .chain Datei im Verzeichnis
@@ -8,30 +8,36 @@ function [N, m, c, y] = loadchain(filename)
 %   c             Federsteifigkeit
 %   y             Anfangsauslenkung
 
+%ACHTUNG! FUNKTIONIERT NICHT MIT LONG.CHAIN!!! Weiß nicht wieso
+
 clc; 
 
 fid = fopen(filename + ".chain", "r");
 
 % 1. Zeile überspringen
+disp("suche N");
 fgetl(fid);
 
 % 2. Zeile ist N, auslesen als float (%f)
-N = fscanf(fid, "%f");
+N = fscanf(fid, "%d");
 
 % 3. Zeile überspringen
+disp("suche m");
 fgetl(fid);        
 
 % 4. Zeile ist m, Anzahl der Zeichen ist N
 m = fscanf(fid, "%f", [1, N]);
 
 % 5. Zeile überspringen
-fgetl(fid);      
+disp("suche c");
+fgetl(fid)    
 
 % 6. Zeile ist c, Anzahl Zeichen = N + 1
 c = fscanf(fid, "%f", [1, N + 1]);
 
 % 7. Überspringen
-fgetl(fid);      
+disp("suche y");
+fgetl(fid)     
 
 % 8. Zeile ist y, Anzahl Zeichen ist N
 y = fscanf(fid, "%f", [1, N]);
