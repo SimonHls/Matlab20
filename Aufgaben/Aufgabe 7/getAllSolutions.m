752dc821ae20c18dd5330ab9b1827cd5be96de48
SimonHls <simon.huelskamp@gmail.com>
2022-02-08 09:59:53 +0100

    aufgabe 7 geht jetzt

--- Aufgaben/Aufgabe 7/getAllSolutions.m
+++ Aufgaben/Aufgabe 7/getAllSolutions.m
@@ -1,26 +1,25 @@
 function arr = getAllSolutions(eqn, x, rng, steps)
-% Findet alle Lösungen für eine Gleichung mit einer Unbekannten eqnin 
-% im Interval rngin. Je höher die Eingabe für steps, desto
-% wahrscheinlicher ist es, alle Lösungen im Interval zu finden.
+% Findet (möglichst) alle Lösungen für eine Gleichung eqn mit einer Unbekannten x 
+% im Interval rng, indem die Lösungen in Teilintervallen (steps) erfasst werden.
 %
 % Parameter:
 %   eqn         Eingabegleichung
-%   x           Variable
+%   x           Unbekannte
 %   rng         Interval für die Lösungssuche
 %   steps       Gibt an, in wie vielen Schritten die Gleichung untersucht wird
 
-%solutions speichert alle gefundenen Lösungen
-solutions = zeros(1, steps);
+% solutions speichert alle gefundenen Lösungen
+solutions = [];
 
-%stepwidth ist die Schrittweite durch das Intervall
+% stepwidth ist die Schrittweite durch das Intervall
 stepwidth = (max(rng) - min(rng)) / steps
 
-%Schreitet in 'steps' Schritten durch das Intervall und speichert nächste
-%Lösung zu diesem Punkt ab.
+% Schreitet in 'steps' Schritten durch das Intervall und speichert nächste
+% Lösung zu diesem Punkt ab.
 for k = 1:steps
-    cs = vpasolve(eqn,x,[stepwidth * k - stepwidth ,stepwidth * k]);
-    disp(cs)
-    if cs ~= 0 
+    cs = vpasolve(eqn,x,[min(rng) + stepwidth * k - stepwidth, min(rng) + stepwidth * k]);
+    if ~isempty(cs)
+        disp(cs);
         solutions(k) = cs;
     end
 end
