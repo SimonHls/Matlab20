b190f8589c96e04c96c00bd5b62045df5d50505a
SimonHls <simon.huelskamp@gmail.com>
2022-03-02 12:03:40 +0100

    geniefaktor erhöht

--- Beispiele aus der Vorlesung/Bilder/BilderSkript.m
+++ Beispiele aus der Vorlesung/Bilder/BilderSkript.m
@@ -1,18 +1,15 @@
 % Öffnet ein Bild, filtert schwarze und weiße Pixel, zeigt das Bild an.
 
-B = imread("bild32.gif");
+% B = imread("bild32.gif");
 % Alternatives Bild:
-% B = imread("bild33.jpg");
+B = imread("bild33.gif");
 
 % Benutzerdefinierte Farbskala (High Contrast)
 scale = (0:255)'/255;
 scale2 = rem((0:255)'/16, 1);
 scale3 = rem((0:255)'/32, 1);
-
 map = [scale, scale2, scale3];
-
 colormap(1 - map); 
-
 image(B);
 
 % colormap("colorcube"); % Ich bevorzuge "colorcube" für bild33.jpg
